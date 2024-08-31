b103fdbf30c63a10da2684c163e9383878c7e41d
Fiqq <yafiqarza@gmail.com>
2024-08-31 12:23:13 +0700

    try_activitySchedule

--- app/src/main/java/com/example/peduliibu_app/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/peduliibu_app/Fragment/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.peduliibu_app.Fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.peduliibu_app.Fragment.ScheduleFragment.ScheduleActivity
 import com.example.peduliibu_app.R
 
 
@@ -21,10 +23,8 @@ class HomeFragment : Fragment() {
 
         val button5 = view.findViewById<Button>(R.id.button5)
         button5.setOnClickListener {
-            Log.d("HomeFragment", "Navigating to CleaningScheduleFragment")
-            findNavController().navigate(R.id.action_homeFragment_to_cleaningScheduleFragment)
-            findNavController().popBackStack()
-            findNavController().navigate(R.id.homeFragment)
+            startActivity(Intent(requireContext(), ScheduleActivity::class.java))
+
         }
 
         return view
